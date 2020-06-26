58f2adf76b243186742f0afe7be6f725eebbabb4
princelewis <princelewis4christ@yahoo.com>
2020-06-26 10:42:13 +0100

    A function that Prints duplicate numbers

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+
+        String[] wordArray = word.split("");
+        Arrays.sort(wordArray);
+
+        int counter = 1;
+
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < wordArray.length -1; i++) {
+            if (wordArray[i] == wordArray[i + 1]) {
+                counter++;
+                continue;
+            }
+            if (counter > 1) {
+                System.out.print(wordArray[i] + " = " + counter);
+            }
+        }
+    }
+}
